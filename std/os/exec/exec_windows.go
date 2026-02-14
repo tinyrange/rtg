dc6f1989b796b83a59b803585b454e315b20d653
Joshua Scarsbrook <jscarsbrook@gmail.com>
2026-02-14 16:48:40 +1000

    add exec.Command to windows

--- std/os/exec/exec_windows.go
+++ std/os/exec/exec_windows.go
@@ -7,6 +7,37 @@ import (
 	"runtime"
 )
 
+type ExitError struct {
+	code int
+}
+
+func (e *ExitError) Error() string {
+	return "exit status " + runtime.IntToString(e.code)
+}
+
+type Cmd struct {
+	Path   string
+	Args   []string
+	Env    []string
+	Stdin  *os.File
+	Stdout *os.File
+	Stderr *os.File
+}
+
+func Command(name string, arg ...interface{}) *Cmd {
+	args := make([]string, 1+len(arg))
+	args[0] = name
+	i := 0
+	for i < len(arg) {
+		args[i+1] = runtime.Tostring(arg[i])
+		i++
+	}
+	return &Cmd{
+		Path: name,
+		Args: args,
+	}
+}
+
 func makeCString(s string) uintptr {
 	n := len(s)
 	ptr := runtime.Alloc(n + 1)
