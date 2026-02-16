c9930d55b9ba44bb3259a96d3bf498faea5d2005
Joshua Scarsbrook <jscarsbrook@gmail.com>
2026-02-17 00:45:34 +1000

    compiler: add disabled-backend stubs for arm64 and win386

--- std/compiler/backend_arm64_stub.go
+++ std/compiler/backend_arm64_stub.go
@@ -15,3 +15,21 @@ func generateLinuxArm64ELF(irmod *IRModule, outputPath string) error {
 func generateWinArm64PE(irmod *IRModule, outputPath string) error {
 	return fmt.Errorf("arm64 backend disabled (built with no_backend_arm64 tag)")
 }
+
+func (g *CodeGen) emitArm64(inst uint32)                            { panic("arm64 backend disabled") }
+func (g *CodeGen) emitAddImm(rd, rn int, imm12 uint32)              { panic("arm64 backend disabled") }
+func (g *CodeGen) emitSubImm(rd, rn int, imm12 uint32)              { panic("arm64 backend disabled") }
+func (g *CodeGen) emitLdr(rt, rn int, offset int)                   { panic("arm64 backend disabled") }
+func (g *CodeGen) emitStr(rt, rn int, offset int)                   { panic("arm64 backend disabled") }
+func (g *CodeGen) emitBlr(rn int)                                   { panic("arm64 backend disabled") }
+func (g *CodeGen) emitMovRRArm64(rd, rm int)                        { panic("arm64 backend disabled") }
+func (g *CodeGen) emitAdrpLdr(rd int, target string, rawOff uint64) { panic("arm64 backend disabled") }
+func (g *CodeGen) patchAdrpAddOrLdr(codeOffset int, pcAddr, targetAddr uint64) {
+	panic("arm64 backend disabled")
+}
+func (g *CodeGen) patchAdrpAdd(codeOffset int, pcAddr, targetAddr uint64) {
+	panic("arm64 backend disabled")
+}
+func (g *CodeGen) patchAdrpLdr(codeOffset int, pcAddr, targetAddr uint64) {
+	panic("arm64 backend disabled")
+}
