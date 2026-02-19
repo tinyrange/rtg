096c84ab0467847a4fdf702abd5a2daae7cf0ea3
Joshua Scarsbrook <jscarsbrook@gmail.com>
2026-02-19 19:17:07 +1000

    stabilize defer smoke test for windows/386

--- tests/04_defer_no_effect.go
+++ tests/04_defer_no_effect.go
@@ -2,6 +2,23 @@ package main
 
 import "fmt"
 
+var order string
+
+func markDefer() {
+	order = order + "d"
+}
+
+func run() {
+	defer markDefer()
+	order = order + "b"
+}
+
 func main() {
-	defer fmt.Print("PASS")
+	order = ""
+	run()
+	if order == "bd" {
+		fmt.Print("PASS")
+		return
+	}
+	panic("defer did not run")
 }
