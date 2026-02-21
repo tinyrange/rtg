4e7c7ca7b25c01a2a4d86acaea00cc7eda255b2a
Joshua Scarsbrook <jscarsbrook@gmail.com>
2026-02-22 00:10:25 +1000

    removed windows/arm64 debugging code

--- std/compiler/backend/aarch64/windows/backend_windows_aarch64.go
+++ std/compiler/backend/aarch64/windows/backend_windows_aarch64.go
@@ -86,16 +86,12 @@ func emitStartArm64Windows(g *aarch64.CodeGen, irmod *ir.IRModule) {
 	g.EmitStp(aarch64.REG_FP, aarch64.REG_LR, aarch64.REG_SP, -16)
 	g.EmitMovRRArm64(aarch64.REG_FP, aarch64.REG_SP)
 
-	emitDebugMarkerArm64(g, 'A')
-
 	// Allocate 16MB operand stack via VirtualAlloc
 	g.EmitMovZ(aarch64.REG_X0, 0, 0)
 	g.EmitLoadImm64Compact(aarch64.REG_X1, 16*1048576)
 	g.EmitLoadImm64Compact(aarch64.REG_X2, 0x3000)
 	g.EmitLoadImm64Compact(aarch64.REG_X3, 0x04)
 	emitCallIATArm64(g, "VirtualAlloc")
-
-	emitDebugMarkerArm64(g, 'B')
 
 	g.EmitLoadImm64Compact(aarch64.REG_X1, 16*1048576)
 	g.EmitAddRR(aarch64.REG_X28, aarch64.REG_X0, aarch64.REG_X1)
@@ -106,11 +102,7 @@ func emitStartArm64Windows(g *aarch64.CodeGen, irmod *ir.IRModule) {
 		}
 	}
 
-	emitDebugMarkerArm64(g, 'C')
-
 	g.EmitCallPlaceholderArm64("main.main")
-
-	emitDebugMarkerArm64(g, 'D')
 
 	g.EmitMovZ(aarch64.REG_X0, 0, 0)
 	emitCallIATArm64(g, "ExitProcess")
@@ -130,29 +122,6 @@ func emitCallIATArm64(g *aarch64.CodeGen, funcName string) {
 	g.EmitArm64(inst)
 	g.AddCallFixup(off, "$iat$"+funcName, 0)
 	g.EmitBlr(aarch64.REG_X16)
-	emitAddImm64Arm64(g, aarch64.REG_SP, aarch64.REG_SP, 32)
-}
-
-func emitDebugMarkerArm64(g *aarch64.CodeGen, marker byte) {
-	if !g.Target().CompilerDebug {
-		return
-	}
-
-	// WriteFile(GetStdHandle(STD_ERROR_HANDLE), &marker, 2, &nwritten, NULL)
-	emitAddImm64Arm64(g, aarch64.REG_SP, aarch64.REG_SP, -32)
-	val := uint64(marker) | (uint64('\n') << 8)
-	g.EmitLoadImm64Compact(aarch64.REG_X1, val)
-	g.EmitStr(aarch64.REG_X1, aarch64.REG_SP, 0)
-
-	g.EmitLoadImm64Compact(aarch64.REG_X0, 0xFFFFFFFFFFFFFFF4) // -12
-	emitCallIATArm64(g, "GetStdHandle")
-
-	g.EmitMovRRArm64(aarch64.REG_X1, aarch64.REG_SP)
-	g.EmitLoadImm64Compact(aarch64.REG_X2, 2)
-	emitAddImm64Arm64(g, aarch64.REG_X3, aarch64.REG_SP, 16)
-	g.EmitMovZ(aarch64.REG_X4, 0, 0)
-	emitCallIATArm64(g, "WriteFile")
-
 	emitAddImm64Arm64(g, aarch64.REG_SP, aarch64.REG_SP, 32)
 }
 
