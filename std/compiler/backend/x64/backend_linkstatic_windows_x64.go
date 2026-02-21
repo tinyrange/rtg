9aa58f4a259971cb3ce033356231a5ca1f38f848
Joshua Scarsbrook <jscarsbrook@gmail.com>
2026-02-22 00:26:59 +1000

    windows: migrate amd64 and 386 runtime syscalls to linkstatic

--- std/compiler/backend/x64/backend_linkstatic_windows_x64.go
+++ std/compiler/backend/x64/backend_linkstatic_windows_x64.go
@@ -34,16 +34,9 @@ func (g *CodeGen) compileLinkStaticIntrinsicWin64(inst ir.Inst) bool {
 	if !ok {
 		panic("ICE: invalid windows linkstatic metadata for '" + inst.Name + "'")
 	}
-	lib = canonicalWinImportLibrary(lib)
-
-	prevActive := g.linkStaticImportActive
-	prevLib := g.linkStaticImportLib
-	prevSym := g.linkStaticImportSymbol
-	g.linkStaticImportActive = true
-	g.linkStaticImportLib = lib
-	g.linkStaticImportSymbol = sym
-
-	knownSymbol := true
+	if lib != "kernel32.dll" {
+		panic("ICE: unsupported windows linkstatic library '" + lib + "'")
+	}
 
 	switch sym {
 	case "ReadFile":
@@ -89,12 +82,6 @@ func (g *CodeGen) compileLinkStaticIntrinsicWin64(inst ir.Inst) bool {
 	case "GetCurrentProcessId":
 		g.compileSyscallGetpid_win64()
 	default:
-		knownSymbol = false
-	}
-	g.linkStaticImportActive = prevActive
-	g.linkStaticImportLib = prevLib
-	g.linkStaticImportSymbol = prevSym
-	if !knownSymbol {
 		panic("ICE: unknown windows linkstatic symbol '" + sym + "'")
 	}
 	return true
