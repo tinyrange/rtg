36320192a1746a0fe0fe6b61ee52b4b0069288f3
Joshua Scarsbrook <jscarsbrook@gmail.com>
2026-02-22 15:13:36 +1000

    tests: stabilize comptime coverage and skip 386 fullcompiler case

--- std/compiler/backend/vm/backend_vm_syscall_default.go
+++ std/compiler/backend/vm/backend_vm_syscall_default.go
@@ -2,6 +2,8 @@
 
 package vm
 
+import "os"
+
 func (vm *VM) execSyscallIntrinsic(num uint64, ws uint64, a0 uint64, a1 uint64, a2 uint64, a3 uint64, a4 uint64, a5 uint64) {
 	switch vm.targetGOARCH {
 	case "amd64":
@@ -77,31 +79,150 @@ func (vm *VM) execSyscallIntrinsic(num uint64, ws uint64, a0 uint64, a1 uint64, 
 		// linux/386 and dos16 use the same numbering for basic file syscalls.
 		switch num {
 		case 3:
-			vm.execIntrinsicArgs("SysRead", ws, a0, a1, a2)
+			fd := int(a0)
+			bufAddr := a1
+			count := a2
+			if fd < 0 || fd >= 256 || !vm.fdUsed[fd] {
+				vm.vmSysReturn(-1)
+				return
+			}
+			n := int(count)
+			buf := make([]byte, n)
+			f := vm.fdFiles[fd]
+			nr, _ := f.Read(buf)
+			if nr > 0 {
+				vm.copyToVM(int(bufAddr), buf, nr)
+			}
+			vm.vmSysReturn(int64(nr))
 		case 4:
-			vm.execIntrinsicArgs("SysWrite", ws, a0, a1, a2)
+			fd := int(a0)
+			bufAddr := a1
+			count := a2
+			if fd < 0 || fd >= 256 || !vm.fdUsed[fd] {
+				vm.vmSysReturn(-1)
+				return
+			}
+			n := int(count)
+			a := int(bufAddr)
+			if a+n > len(vm.memory) {
+				n = len(vm.memory) - a
+			}
+			if n <= 0 {
+				vm.vmSysReturn(0)
+				return
+			}
+			f := vm.fdFiles[fd]
+			nw, err := f.Write(vm.memory[a : a+n])
+			if err != nil {
+				vm.vmSysReturn(-1)
+			} else {
+				vm.vmSysReturn(int64(nw))
+			}
 		case 5:
-			vm.execIntrinsicArgs("SysOpen", ws, a0, a1, a2)
+			path := vm.readCString(a0)
+			fl := int(a1)
+			var flag int
+			if fl&1 != 0 {
+				flag = os.O_WRONLY | os.O_CREATE | os.O_TRUNC
+			} else if fl&2 != 0 {
+				flag = os.O_RDWR
+			} else {
+				flag = os.O_RDONLY
+			}
+			f, err := os.OpenFile(path, flag, 0644)
+			if err != nil {
+				vm.vmSysReturn(-2)
+				return
+			}
+			fd := vm.nextFD
+			vm.nextFD = vm.nextFD + 1
+			if fd >= 256 {
+				f.Close()
+				vm.vmSysReturn(-1)
+				return
+			}
+			vm.fdFiles[fd] = f
+			vm.fdUsed[fd] = true
+			vm.vmSysReturn(int64(fd))
 		case 6:
-			vm.execIntrinsicArgs("SysClose", ws, a0)
+			fd := int(a0)
+			if fd < 3 || fd >= 256 || !vm.fdUsed[fd] {
+				vm.vmSysReturn(-1)
+				return
+			}
+			f := vm.fdFiles[fd]
+			f.Close()
+			vm.fdFiles[fd] = nil
+			vm.fdUsed[fd] = false
+			vm.vmSysReturn(0)
 		case 10:
-			vm.execIntrinsicArgs("SysUnlink", ws, a0)
+			path := vm.readCString(a0)
+			err := os.RemoveAll(path)
+			if err != nil {
+				vm.vmSysReturn(-1)
+			} else {
+				vm.vmSysReturn(0)
+			}
 		case 15:
-			vm.execIntrinsicArgs("SysChmod", ws, a0, a1)
+			path := vm.readCString(a0)
+			err := os.Chmod(path, os.FileMode(a1))
+			if err != nil {
+				vm.vmSysReturn(-1)
+			} else {
+				vm.vmSysReturn(0)
+			}
 		case 20:
-			vm.execIntrinsicArgs("SysGetpid", ws)
+			vm.push(uint64(os.Getpid()) & vm.config.WordMask)
+			vm.push(0)
+			vm.push(0)
 		case 39:
-			vm.execIntrinsicArgs("SysMkdir", ws, a0, a1)
+			path := vm.readCString(a0)
+			err := os.MkdirAll(path, 0755)
+			if err != nil {
+				vm.vmSysReturn(-17)
+			} else {
+				vm.vmSysReturn(0)
+			}
 		case 40:
-			vm.execIntrinsicArgs("SysRmdir", ws, a0)
+			path := vm.readCString(a0)
+			err := os.RemoveAll(path)
+			if err != nil {
+				vm.vmSysReturn(-1)
+			} else {
+				vm.vmSysReturn(0)
+			}
 		case 106:
-			vm.execIntrinsicArgs("SysStat", ws, a0, a1)
+			path := vm.readCString(a0)
+			f, err := os.Open(path)
+			if err == nil {
+				f.Close()
+				vm.vmSysReturn(0)
+			} else {
+				vm.vmSysReturn(-2)
+			}
 		case 183:
-			vm.execIntrinsicArgs("SysGetcwd", ws, a0, a1)
+			cwd, err := os.Getwd()
+			if err != nil {
+				vm.vmSysReturn(-1)
+				return
+			}
+			n := len(cwd)
+			if n >= int(a1) {
+				n = int(a1) - 1
+			}
+			vm.copyStringToVM(int(a0), cwd, n)
+			vm.memory[int(a0)+n] = 0
+			vm.vmSysReturn(int64(n))
 		case 192:
-			vm.execIntrinsicArgs("SysMmap", ws, a0, a1, a2, a3, a4, a5)
+			size := a1
+			if size == 0 {
+				size = 1
+			}
+			addr := vm.alloc(size, "mmap")
+			vm.vmSysReturn(int64(addr))
 		case 252:
-			vm.execIntrinsicArgs("SysExit", ws, a0)
+			ExitCode = int(vm.signExtend(a0))
+			vm.exited = true
 		default:
 			vm.vmSysReturn(-38)
 		}
