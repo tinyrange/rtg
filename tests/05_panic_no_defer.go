f075d86f91047fb91a6aaf152af75b171bc659ea
Joshua Scarsbrook <jscarsbrook@gmail.com>
2026-02-24 15:20:46 +1000

    frontend: implement Go-compatible dynamic defer semantics

--- tests/05_panic_no_defer.go
+++ tests/05_panic_no_defer.go
@@ -5,12 +5,28 @@ import (
 	"os"
 )
 
+var panicOrder string
+
+func appendPanicDigit(n int) {
+	panicOrder = panicOrder + string([]byte{byte('0' + n)})
+}
+
 func done() {
-	fmt.Print("PASS")
-	os.Exit(0)
+	if panicOrder == "21" {
+		fmt.Print("PASS")
+		os.Exit(0)
+	}
+	fmt.Printf("FAIL: panic defer order=%s\n", panicOrder)
+	os.Exit(1)
 }
 
 func main() {
+	panicOrder = ""
 	defer done()
+	i := 1
+	for i <= 2 {
+		defer appendPanicDigit(i)
+		i++
+	}
 	panic("boom")
 }
